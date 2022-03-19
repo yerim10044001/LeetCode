3ef186b91b17b5be71af00c9e0325ab8ad98e028
Ye Rim Lee <86493874+yerim10044001@users.noreply.github.com>
2022-03-19 21:42:27 +0900

    Time: 217 ms (6.15%), Space: 14.2 MB (39.91%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,16 +1,15 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
         m = len(nums1)
         n = len(nums2)
-        
+
         #even odd check
         if (m+n)%2:
             case = 1    # answer is one
         else:
-            case = 2    # answer = a + b / 2
+            case = 2    # answer is two  a + b / 2
 
-        # check list size
+        # make nums1 always bigger than nums2
         if m<n:
             temp = list(nums2)
             nums2 = list(nums1)
@@ -19,9 +18,9 @@ class Solution:
             n = m
             m = t1
 
-        ###3 solution ####
-        m_index = (m+n-1)//2
 
+        ### solution ####
+        m_index = (m+n-1)//2
         ######   case m>n   ######
         # i <- nums1 index
         # j <- nums2 index
@@ -31,9 +30,9 @@ class Solution:
         i = m_index - j
 
         # find middle index
-        # binary search start
+        # binary search for index sum     [] + [] = middle index
         find = 1
-        
+
         # if m, n == 0 exception
         if n == 0:
             answer1 = nums1[m_index]
@@ -47,8 +46,10 @@ class Solution:
             answer1 = nums1[0]
             answer2 = nums2[0]
             find = 0
-            
+
+        # binary search start
         while(find):
+            # for exception
             if j<0:
                 answer1 = nums1[m_index]
                 if m_index == m-1:
@@ -56,7 +57,6 @@ class Solution:
                 else:
                     answer2 = nums1[m_index+1]
                 break
-                
             if left>right:
                 answer1 = nums1[m_index-n]
                 if case ==1:   
@@ -64,9 +64,10 @@ class Solution:
                 else:
                     answer2 = nums1[m_index-n+1]
                 break
+            ###
 
             if (nums1[i] >= nums2[j]):
-                if (nums1[i-1]<=nums2[j])or i-1<0:
+                if (nums1[i-1]<=nums2[j]) or i-1<0:
                     answer1 = nums2[j]
                     if (j == n-1) or (nums1[i]<nums2[j+1]):
                         answer2 = nums1[i]
@@ -74,13 +75,13 @@ class Solution:
                         answer2 = nums2[j+1]
                     find = 0
                     break
-                else:
+                else:   #search right
                     left = j+1
                     j = (right+left)//2
                     i = m_index - j
                     continue        
             else:
-                if (nums1[i] >= nums2[j-1])or j-1<0:
+                if (nums1[i] >= nums2[j-1]) or j-1<0:
                     answer1 = nums1[i]
                     if (i == m-1) or (nums2[j]<nums1[i+1]):
                         answer2 = nums2[j]
@@ -88,15 +89,18 @@ class Solution:
                         answer2 = nums1[i+1]
                     find = 0
                     break
-                else:
+                else:   #search left
                     right = j-1
                     j = (right+left)//2
                     i = m_index - j
                     continue
 
-
+        ##########
+        # output #
         if case == 1:
-            return (answer1)
+            return(answer1)
         else:
-            return ((answer1+answer2)/2)
+            return((answer1+answer2)/2)
+
+
 
